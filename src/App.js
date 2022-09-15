7bda20f6a795e72e1a1ccb3edf8ab7a536ffa011
Wesley Dias <wesley.dias.dev@gmail.com>
2022-09-15 12:18:38 -0500

    Separando os componentes da apĺicação

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Corpo from './Corpo'
+import Mobile from './Mobile'
+import Navbar from './Navbar'
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Corpo/>
+      <Mobile/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Corpo.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Posts from "./Posts";
+import SideBar from "./SideBar";
+
+function Corpo() {
+  return (
+    <div class="corpo">
+      <Posts />
+      <SideBar />
+    </div>
+  );
+}
+
+export default Corpo;
