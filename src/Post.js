fce144b61fda209c0da55e118d7cf8a903f76c70
Wesley Dias <wesley.dias.dev@gmail.com>
2022-09-16 08:06:26 -0500

    Inserido alguns componentes que ainda estou alterando, e finalizado o componente de Navbar

--- src/Post.js
+++ src/Post.js
@@ -1,21 +1,12 @@
 import React from 'react'
-import Post from './Post'
-import Stories from './Stories'
 
-function Posts() {
+function Post(props) {
   return (
-    <div>
-      <div class="esquerda">
-        <Stories/>
-
-        <div class="posts">
-          <Post nome={'wesley'} srcPerfil={'assets/img/meowed.svg'} srcPost={'assets/img/gato-telefone.svg'}/>
-
-          <div class="post">
+    <div class="post">
             <div class="topo">
               <div class="usuario">
-                <img src="assets/img/barked.svg" alt='imagem-usuario-post'/>
-                barked
+                <img src={props.srcPerfil} alt='foto-do-post'/>
+                {props.nome}
               </div>
               <div class="acoes">
                 <ion-icon name="ellipsis-horizontal"></ion-icon>
@@ -23,7 +14,7 @@ function Posts() {
             </div>
 
             <div class="conteudo">
-              <img src="assets/img/dog.svg" alt='imagem-post'/>
+              <img src={props.srcPost} alt='foto-do-post'/>
             </div>
 
             <div class="fundo">
@@ -39,17 +30,14 @@ function Posts() {
               </div>
 
               <div class="curtidas">
-                <img src="assets/img/adorable_animals.svg" alt='imagem-perfil-curtida'/>
+                <img src="assets/img/respondeai.svg" alt='foto-curtida'/>
                 <div class="texto">
-                  Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>
+                  Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
-    </div>
   )
 }
 
-export default Posts
+export default Post
