b12ec623b8ceda22adf40542cdeadad2a1dc15c5
Wesley Dias <wesley.dias.dev@gmail.com>
2022-09-16 08:43:57 -0500

    Estou renderizando cada Story utilizando map

--- src/Stories.js
+++ src/Stories.js
@@ -2,17 +2,22 @@ import React from 'react'
 import Story from './Story'
 
 function Stories() {
+
+  const todosStories = [
+    {imagemPerfil: 'assets/img/9gag.svg',user:'9gag'}, 
+    {imagemPerfil: 'assets/img/meowed.svg',user:'eowed'},
+    {imagemPerfil: 'assets/img/barked.svg',user:'barked'},
+    {imagemPerfil: 'assets/img/nathanwpylestrangeplanet.svg',user:'nathanwpylestrangeplanet'},
+    {imagemPerfil: 'assets/img/wawawicomics.svg',user:'wawawicomics'},
+    {imagemPerfil: 'assets/img/respondeai.svg',user:'respondeai'},
+    {imagemPerfil: 'assets/img/filomoderna.svg',user:'filomoderna'},
+    {imagemPerfil: 'assets/img/memeriagourmet.svg',user:'memeriagourmet'}
+]
+
+
   return (
     <div class="stories">
-    <Story imagemPerfil={"assets/img/9gag.svg"} user={'9gag'}/>
-    <Story imagemPerfil={"assets/img/meowed.svg"} user={'meowed'}/>
-    <Story imagemPerfil={"assets/img/barked.svg"} user={'barked'}/>
-    <Story imagemPerfil={"assets/img/nathanwpylestrangeplanet.svg"} user={'nathanwpylestrangeplanet'}/>
-    <Story imagemPerfil={"assets/img/wawawicomics.svg"} user={'wawawicomics'}/>
-    <Story imagemPerfil={"assets/img/respondeai.svg"} user={'respondeai'}/>
-    <Story imagemPerfil={"assets/img/filomoderna.svg"} user={'filomoderna'}/>
-    <Story imagemPerfil={"assets/img/memeriagourmet.svg"} user={'memeriagourmet'}/>
-
+      {todosStories.map((s) => <Story imagemPerfil={s.imagemPerfil} user={s.user}/>)}
     <div class="setinha">
       <ion-icon name="chevron-forward-circle"></ion-icon>
     </div>
