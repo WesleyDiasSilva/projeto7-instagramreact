ea25f67f2d9f71f1c349ee2b887eb6dfc5b2216e
Wesley Dias <wesley.dias.dev@gmail.com>
2022-09-16 11:37:41 -0500

    Adicionei a funcionalidade de poder trocar a foto do perfil com uma url externa

--- src/Usuario.js
+++ src/Usuario.js
@@ -3,18 +3,22 @@ import React from 'react'
 function Usuario(props) {
 
   let [nomeUsuario, setNomeUsuario] = React.useState()
+  let [fotoUsuario, setFotoUsuario] = React.useState()
 
   function mudaNome(){
     let nome = prompt('Qual é o seu nome?');
     setNomeUsuario(nome)
   }
 
-
+  function mudaFoto(){
+    let foto = prompt('Preencha com o link de uma imagem por favor!');
+    setFotoUsuario(foto)
+  }
   
 
   return (
       <div class="usuario">
-          <img src="assets/img/catanacomics.svg" alt="imagem-perfil-usuario"/>
+          <img onClick={() => mudaFoto()} src={fotoUsuario ? fotoUsuario : "assets/img/catanacomics.svg"} alt="imagem-perfil-usuario"/>
           <div class="texto">
             <strong>catanacomics</strong>
             <span>
