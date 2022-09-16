72913ea7089345ee17632a1d6a1338971cd745b7
Wesley Dias <wesley.dias.dev@gmail.com>
2022-09-16 11:04:19 -0500

    Adicionei um array com os posts para serem renderizados no projeto utilizando map

--- src/Posts.js
+++ src/Posts.js
@@ -3,15 +3,19 @@ import Post from './Post'
 import Stories from './Stories'
 
 function Posts() {
+
+  const todosPosts = [
+    {nome:'meowed',srcPerfil:'assets/img/meowed.svg',srcPost:'assets/img/gato-telefone.svg',curtidoPor:'respondeAí',numeroLike:'outras 101.523 pessoas'},
+    {nome:'barked',srcPerfil:'assets/img/barked.svg',srcPost:'assets/img/dog.svg',curtidoPor:'adorable_animals',numeroLike:'outras 99.159 pessoas'}
+  ]
+
   return (
     <div>
       <div class="esquerda">
         <Stories/>
-
         <div class="posts">
-          <Post nome={'meowed'} srcPerfil={'assets/img/meowed.svg'} srcPost={'assets/img/gato-telefone.svg'} curtidoPor={'respondeAí'} numeroLike={'outras 101.523 pessoas'}/>
+          {todosPosts.map((p) => <Post nome={p.nome} srcPerfil={p.srcPerfil} srcPost={p.srcPost} curtidoPor={p.curtidoPor} numeroLike={p.numeroLike}/>)}
 
-          <Post nome={'barked'} srcPerfil={'assets/img/barked.svg'} srcPost={'assets/img/dog.svg'} curtidoPor={'adorable_animals'} numeroLike={'outras 99.159 pessoas'} />
         </div>
       </div>
     </div>
