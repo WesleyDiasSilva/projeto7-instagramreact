6377f30892a6d3d326ab55dddaa4cdfdef0ce12d
Wesley Dias <wesley.dias.dev@gmail.com>
2022-09-16 11:10:29 -0500

    Estou renderizando cada sugestão utilzando um array de sugestões e criando cada sugestão com map

--- src/Sugestoes.js
+++ src/Sugestoes.js
@@ -2,18 +2,43 @@ import React from 'react'
 import Sugestao from './Sugestao'
 
 function Sugestoes() {
+
+  const todasSugestoes= [
+    {
+      imagemUser:'assets/img/bad.vibes.memes.svg',
+      nomeUser:'bad.vibes.memes',
+      statusUser:"Segue você"
+    },
+    {
+      imagemUser:'assets/img/chibirdart.svg',
+      nomeUser:'chibirdart',
+      statusUser:"Segue você"
+    },
+    {
+      imagemUser:'assets/img/razoesparaacreditar.svg',
+      nomeUser:'razoesparaacreditar',
+      statusUser:"Novo no Instagram"
+    },
+    {
+      imagemUser:'assets/img/adorable_animals.svg',
+      nomeUser:'adorable_animals',
+      statusUser:"Segue você"
+    },
+    {
+      imagemUser:'assets/img/smallcutecats.svg',
+      nomeUser:'smallcutecats',
+      statusUser:"Segue você"
+    }
+  ]
+
   return (
       <div class="sugestoes">
           <div class="titulo">
             Sugestões para você
             <div>Ver tudo</div>
           </div>
-
-          <Sugestao imagemUser='assets/img/bad.vibes.memes.svg' nomeUser='bad.vibes.memes' statusUser="Segue você"/>
-          <Sugestao imagemUser='assets/img/chibirdart.svg' nomeUser='chibirdart' statusUser="Segue você"/>
-          <Sugestao imagemUser='assets/img/razoesparaacreditar.svg' nomeUser='razoesparaacreditar' statusUser="Novo no Instagram"/>
-          <Sugestao imagemUser='assets/img/adorable_animals.svg' nomeUser='adorable_animals' statusUser="Segue você"/>
-          <Sugestao imagemUser='assets/img/smallcutecats.svg' nomeUser='smallcutecats' statusUser="Segue você"/>
+            {todasSugestoes.map((s) => <Sugestao imagemUser={s.imagemUser} nomeUser={s.nomeUser} statusUser={s.statusUser}/>)}
+          
         </div>
   )
 }
